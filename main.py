e486455869f106a0ad1d12bb542d44dec7ffd694
Nathan Rafael Ramos <ramosnathanrafael@gmail.com>
2024-11-17 20:11:38 +0800

    Cleaned up main() & movement() in main.py

--- main.py
+++ main.py
@@ -10,39 +10,92 @@ def clear_screen():
         subprocess.run([clear_cmd])
 
 
-def main(): 
+def main(): # main() just sets the level up, what the info is, all that jazz
     if len(sys.argv) < 2:
-        print('The game requires a filename to start.', file = sys.stderr)
+        print('No level detected.', file = sys.stderr)
         return 
     with open(sys.argv[1], encoding = 'utf-8-sig') as f:
         clear_screen()
         lengths = {'rows': int(f.readline()) - 1} # 0 to r - 1
-        move_set = {'moves_left': int(f.readline()), 'your_move': '', 'current_move': '', 'moved_eggs': []}
+        move_set = {
+            'moves_left': int(f.readline()), 
+            'your_move': '', 
+            'current_move': '', 
+            'moved_eggs': []
+        }
         start_level = f.read()
-        level_info = {'level': start_level.split('\n')[:-1], 'points': 0, 'switch': True, 'eggs_in_field': True}
-        print('\n'.join(level_info['level']))
+        level_info = {
+            'level': start_level.split('\n')[:-1], 
+            'points': 0, 
+            'switch': True, 
+            'eggs_in_field': True # switch that determines if there are eggs in field
+        }
         lengths.update({'cols': len(level_info['level'][0]) - 1}) # 0 to c - 1
-        movement(lengths, move_set, level_info)
+        # calling a start() function would likely be better than calling movement() from the get-go.
+
+        return start(lengths, move_set, level_info)
 
 
+def start(lengths, move_set, level_info):
+    print('\n'.join(level_info['level'])) # shows the level during the beginning.
+    # should go to movement() after this. 
+    movement(lengths, move_set, level_info)
+    clear_screen()
+    time.sleep(0.5)
+    return game_over(lengths, move_set, level_info)
+
+
+# break movement function into smaller, unit-testable functions
+# explain the movement() function & see how i can split it apart
+# movement gets user input
+# movement checks if move is valid
+# movement also checks the game state end conditions
 def movement(lengths, move_set, level_info): # detects user input, what interacts with player
-    possible_moves = set(('l', 'r', 'f', 'b'))
-    print(f'Previous Moves: {move_set['your_move']}')
-    print(f'Remaining Moves: {move_set['moves_left']}')
-    print(f'Points: {level_info['points']}')
-    while move_set['moves_left'] > 0 and level_info['eggs_in_field']: # other condition is for later
-        input_move = input('Enter move/s: ')
-        if input_move.lower() in possible_moves:
-            move_set['current_move'] += input_move.lower()
+    while move_set['moves_left'] > 0 and level_info['eggs_in_field']:
+        print(ui(lengths, move_set, level_info))
+        move_set['current_move'] += player_input()
+        if movement_check(move_set['current_move']): # redirects to an outside function to test
+            return screen(lengths, move_set, level_info)
         else:
-
+            move_set['current_move'] = ''
             print('Invalid Move, clearing...')
             time.sleep(0.4)
+            clear_screen()
+            print('\n'.join(level_info['level']))
 
-        screen(lengths, move_set, level_info) 
-        # goes to screen() to modify the game state based on user input
-    final_state(lengths, move_set, level_info)
-    # technically, it shows the final game state already once it reaches 0 moves, but this is cleaner.
+
+def player_input():
+    """
+    player_input() is the main function that interacts with the player
+
+    it detects the player's input and returns the input() to the current_move key in move_set
+    """
+    input_move = input('Enter move/s: ')
+    return input_move
+
+
+def movement_check(current_move): 
+    """
+    This is a switch that detect whether or not the move is valid or not.
+
+    If the move is valid, it returns True
+    If the move is invalid, it returns False
+
+    Might expand this later to encompass chain moves & undo.
+    """
+    possible_moves = set(('l', 'r', 'f', 'b'))
+    if current_move.lower() not in possible_moves:
+        return False
+    else:
+        return True
+
+
+def ui(lengths, move_set, level_info): 
+    # separated the UI from the movement function, why? i have no idea myself.
+    previous = f'Previous Moves: {move_set['your_move']}'
+    remaining = f'Remaining Moves: {move_set['moves_left']}'
+    ui_points = f'Points: {level_info['points']}'
+    return '\n'.join((previous, remaining, ui_points))
 
 
 # i want to add a somewhat auto-feature where if you type consecutive characters, the game moves on its own
